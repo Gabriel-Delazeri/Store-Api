db0122ffd59a270263e01762dbd06487c0aa50d4
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-03-02 22:15:27 -0300

    relationship between orders and users

--- src/main/java/com/delazeri/course/entities/User.java
+++ src/main/java/com/delazeri/course/entities/User.java
@@ -1,14 +1,16 @@
 package com.delazeri.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -18,6 +20,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -68,6 +74,18 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void addOrder(Order order) {
+        //TODO
+    }
+
+    public void removeOrder(Order order){
+        //TODO
     }
 
     @Override
---
+++ src/main/java/com/delazeri/course/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.delazeri.course.services;
+
+import com.delazeri.course.entities.Order;
+import com.delazeri.course.entities.User;
+import com.delazeri.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> order = orderRepository.findById(id);
+
+        return order.get();
+    }
+}
+
