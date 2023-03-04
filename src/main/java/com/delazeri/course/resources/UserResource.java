0293a1414374ce80af32aea51a721070a529df87
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-03-04 13:37:55 -0300

    create user endpoint

--- src/main/java/com/delazeri/course/resources/UserResource.java
+++ src/main/java/com/delazeri/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.delazeri.course.entities.User;
 import com.delazeri.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,5 +25,17 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> getById(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User obj) {
+        User user = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(user);
     }
 }
