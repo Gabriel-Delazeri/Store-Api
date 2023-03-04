50e5106745b6121a3a1c04ddeef5eb004329ada6
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-03-04 14:16:40 -0300

    user not found exception

--- src/main/java/com/delazeri/course/services/UserService.java
+++ src/main/java/com/delazeri/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.delazeri.course.services;
 
 import com.delazeri.course.entities.User;
 import com.delazeri.course.repositories.UserRepository;
+import com.delazeri.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,7 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
 
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
