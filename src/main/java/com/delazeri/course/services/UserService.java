7a90933f9fb9e7bf8eebdb9188e8d1a251cf6033
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-03-04 14:27:30 -0300

    delete exception handling

--- src/main/java/com/delazeri/course/services/UserService.java
+++ src/main/java/com/delazeri/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.delazeri.course.services;
 
 import com.delazeri.course.entities.User;
 import com.delazeri.course.repositories.UserRepository;
+import com.delazeri.course.services.exceptions.DatabaseException;
 import com.delazeri.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException exception) {
+            throw new DatabaseException(exception.getMessage());
+        }
     }
 
     public User update(Long id, User updatedUser) {
